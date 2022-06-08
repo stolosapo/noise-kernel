dd03bf74ed9bedcb62eaa131f7b5d16354d21de8
Apostolos <aldrigmej@yahoo.gr>
2022-06-08 22:38:39 +0300

    Reformat file

--- src/argument/ArgumentParser.cpp
+++ src/argument/ArgumentParser.cpp
@@ -20,32 +20,32 @@ ArgumentParser::~ArgumentParser()
 
 bool ArgumentParser::isCommand(string arg)
 {
-	int size = arg.size();
-	int pref_size = (int) strlen(COMMAND_PREFIX);
+    int size = arg.size();
+    int pref_size = (int) strlen(COMMAND_PREFIX);
 
-	if (size <= pref_size)
-	{
-		return false;
-	}
+    if (size <= pref_size)
+    {
+        return false;
+    }
 
-	for (int i = 0; i < pref_size; ++i)
-	{
-		if (COMMAND_PREFIX[i] != arg[i])
-		{
-			return false;
-		}
-	}
+    for (int i = 0; i < pref_size; ++i)
+    {
+        if (COMMAND_PREFIX[i] != arg[i])
+        {
+            return false;
+        }
+    }
 
-	return true;
+    return true;
 }
 
 string ArgumentParser::trimCommand(string command)
 {
-	int size = command.size();
-	int pref_size = (int) strlen(COMMAND_PREFIX);
-	int len = size - pref_size;
+    int size = command.size();
+    int pref_size = (int) strlen(COMMAND_PREFIX);
+    int len = size - pref_size;
 
-	return command.substr(pref_size, len);
+    return command.substr(pref_size, len);
 }
 
 ArgumentList ArgumentParser::parse(int argc, char* argv[])
@@ -53,27 +53,27 @@ ArgumentList ArgumentParser::parse(int argc, char* argv[])
     vector<Argument> arguments;
 
     if (argc == 0)
-	{
-		return ArgumentList(arguments);
-	}
+    {
+        return ArgumentList(arguments);
+    }
 
-	string execName = argv[0];
+    string execName = argv[0];
 
-	if (argc == 1)
-	{
-		return ArgumentList(arguments);
-	}
+    if (argc == 1)
+    {
+        return ArgumentList(arguments);
+    }
 
-	string prev_command = "";
+    string prev_command = "";
     bool is_prev_command = false;
 
-	for (int i = 1; i < argc; ++i)
-	{
-		string arg(argv[i]);
-		bool is_command = isCommand(arg);
+    for (int i = 1; i < argc; ++i)
+    {
+        string arg(argv[i]);
+        bool is_command = isCommand(arg);
 
-		if (is_command)
-		{
+        if (is_command)
+        {
             if (is_prev_command)
             {
                 Argument a = Argument(prev_command, "");
@@ -87,28 +87,28 @@ ArgumentList ArgumentParser::parse(int argc, char* argv[])
                 Argument a = Argument(prev_command, "");
                 arguments.push_back(a);
             }
-		}
-		else
-		{
+        }
+        else
+        {
             Argument a = Argument(prev_command, arg);
             arguments.push_back(a);
-		}
+        }
 
         is_prev_command = is_command;
-	}
+    }
 
     return ArgumentList(arguments);
 }
 
 void ArgumentParser::print(int argc, char* argv[])
 {
-	ArgumentList arg_list = parse(argc, argv);
+    ArgumentList arg_list = parse(argc, argv);
 
     vector<Argument> list = arg_list.getArguments();
 
     for (int i = 0; i < list.size(); ++i)
-	{
+    {
         Argument a = list.at(i);
-		cout << i << ") " << a.getName() << " = " << a.getValue() << endl;
-	}
+        cout << i << ") " << a.getName() << " = " << a.getValue() << endl;
+    }
 }
