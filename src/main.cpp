f873a5dfe1000c285d4e2ec478b05385ac81be86
Apostolos Krionidis <akrionidis@vivantehealth.com>
2022-06-09 11:32:02 +0300

    Added example in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <Argument.h>
 
 using namespace std;
 
 int main(int argc, char* argv[])
 {
     cout << "noisekernel Library" << endl << endl;
+
+    NoiseKernel::ArgumentParser parser;
+    parser.print(argc, argv);
+
     cout << "Bye Bye.." << endl;
 }
