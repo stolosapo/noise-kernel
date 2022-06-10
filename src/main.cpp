e51a97fd7cd57fb045bd1971a965285ad0bd8f5c
Apostolos Krionidis <akrionidis@vivantehealth.com>
2022-06-10 16:03:43 +0300

    Added main example

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Argument.h"
+#include "Signal.h"
 #include "argument/MockAdapter.h"
 
 using namespace std;
@@ -11,8 +12,14 @@ int main(int argc, char* argv[])
     NoiseKernel::ArgumentProvider provider(argc, argv);
     MockAdapter adapter(&provider);
     adapter.registerArguments();
-
     cout << adapter.help() << endl;
+
+    NoiseKernel::SignalAdapter signalAdapter;
+    signalAdapter.registerSignals();
+    signalAdapter.setup();
+    cout << "Got SIGINT: " << signalAdapter.gotSigInt() << endl;
+    raise(SIGINT);
+    cout << "Got SIGINT and reset it: " << signalAdapter.gotSigIntAndReset() << endl;
 
     cout << "Bye Bye.." << endl;
 }
